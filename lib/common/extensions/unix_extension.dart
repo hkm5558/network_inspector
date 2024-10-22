e6a105a2f3810640040998e0bc251d439e7b22c7
km <szhuangkm@163.com>
2024-10-22 14:26:10 +0800

    fix: 修复显示时间格式化不正确的问题

--- lib/common/extensions/unix_extension.dart
+++ lib/common/extensions/unix_extension.dart
@@ -3,7 +3,7 @@ import 'package:intl/intl.dart';
 extension UnixExtension on int? {
   String? get convertToYmdHms {
     if (this != null) {
-      var dateFormat = DateFormat('y-MM-DD hh:mm:ss');
+      var dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
       var dateTime = DateTime.fromMillisecondsSinceEpoch(this!);
       return dateFormat.format(dateTime);
     }
